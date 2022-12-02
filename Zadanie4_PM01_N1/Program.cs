6a7e5a7b7f34998a8ab1cd7773b50fd6a743354f
SaveLuciferR <cmudrec@mail.ru>
2022-12-02 22:30:50 +0300

    Добавьте файлы проекта.

---
+++ Zadanie4_PM01_N1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variant10
+{
+	class Program
+	{
+		static void Main()
+		{
+			for (int i = 1; i <= 5; i++, Console.WriteLine())
+			{
+				for (int j = 1; j <= i; j++)
+				{
+					Console.Write(" " + j);
+				}
+			}
+		}
+	}
+}
